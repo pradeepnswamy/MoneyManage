3ed21737783b7af129a3d010f9a40135c268d6af
Pradeep <pradeepnswamy@gmail.com>
2017-07-19 23:42:03 +0530

    committing the whole new project

---
+++ MoneyManage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MoneyManage
+//
+//  Created by Pradeep on 19/07/17.
+//  Copyright © 2017 Pradeep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ MoneyManageTests/MoneyManageTests.m
@@ -0,0 +1,39 @@
+//
+//  MoneyManageTests.m
+//  MoneyManageTests
+//
+//  Created by Pradeep on 19/07/17.
+//  Copyright © 2017 Pradeep. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MoneyManageTests : XCTestCase
+
+@end
+
+@implementation MoneyManageTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
